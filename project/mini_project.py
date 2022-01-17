0062ac86a392eb341a89ace6cbe0d608725a19b0
Monxun <michaelgibney1@gmail.com>
2022-01-17 16:56:46 -0700

    initial commit

---
+++ project/mini_project.py
@@ -0,0 +1,149 @@
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from datetime import datetime
+import logging
+import os
+import streamlit as st
+import pandas as pd
+from services import (
+    files, 
+    workbooks, 
+    logging, 
+    months, 
+    years, 
+    check_file,
+    get_current, 
+    get_summary, 
+    get_voc,
+    log_summary, 
+    log_voc,
+    show_summary, 
+    show_voc,
+    show_logs
+)
+
+#####################################################
+#NAVIGATION
+
+st.image('./spreadsheet-icon.png', width=100)
+st.title('Spreadsheet Mini-Project')
+pages = ['Select by File', 'Current Month', 'Log File']
+navigation = st.selectbox('Select a page', pages)
+
+st.write('_' * 30)
+
+st.header(navigation)
+
+data_flag = False
+
+if navigation != 'Log File':
+
+    if navigation == 'Select by File':
+
+        #####################################################
+        #SELECT FILE
+
+        # workbook file selectbox 
+        workbook_selector = st.selectbox('Select a file', workbooks)  # replace with streamlit selector 
+
+        # verify file exists / error handling
+        check_file(workbook_selector)
+
+        # if file selected display text
+        if workbook_selector:
+            st.text(f'({workbook_selector} is selected.)')
+            logging.info(f'({workbook_selector} is selected.)')
+
+        # initialize workbook
+        wb = load_workbook(f'data/{workbook_selector}')
+
+        # grab month and year from file selection
+        month_year = [item for item in workbook_selector.replace('.', '_').split("_") if item.capitalize() in months or item in years]
+
+        # display month / year
+        st.text(f'Month: {month_year[0].capitalize()}')
+        st.text(f'Year: {month_year[1]}')
+
+        # format month year for datetime comparison
+        month_year_format = f'{month_year[1]}-{months[month_year[0].capitalize()]}'
+
+        # data flag
+
+        data_flag = True
+
+        st.write('_' * 30)
+        st.subheader(f'Data for {month_year[0].capitalize()} - {month_year[1]}')
+
+
+    if navigation == 'Current Month':
+
+        #####################################################
+        #CURRENT MONTH
+
+        data_flag = False
+
+        month, month_word, year = get_current()
+        st.write(f'{month_word}, {year}')
+        current_files = []
+
+        for book in workbooks:
+            if month or month_word and year in book.replace('.', '_').split("_"):
+                current_files.append(book)
+                # data_flag = True
+                
+
+        if current_files:
+            # initialize workbook
+            wb = load_workbook(f'data/{current_files[0]}')
+
+        st.subheader(f'Data for {month_word} - {year}')
+
+
+    if navigation == 'Select by File' or data_flag == True:
+
+        #####################################################
+        #DATA
+
+        # grab worksheet
+        sheets = ['Summary Rolling MoM', 'VOC Rolling MoM']
+        worksheet_selector = st.selectbox('Select a sheet', sheets)
+        ws = wb[worksheet_selector]
+
+        if worksheet_selector == 'Summary Rolling MoM':
+            st.subheader('Summary')
+
+            # get row data and return dictionary
+            row_data = get_summary(ws, month_year_format)
+
+            # log data
+            log_summary(row_data)
+
+            # show data
+            show_summary(row_data)
+
+
+        if worksheet_selector == 'VOC Rolling MoM':
+            st.subheader('VOC')
+
+            # get column data and return dictionary
+            col_data = get_voc(ws, month_year_format)
+
+            # log data
+            log_voc(col_data)
+
+            # show data
+            show_voc(col_data)
+
+    else:
+        st.subheader('NO MATCHING FILES')
+
+else:
+
+    #####################################################
+    #LOG FILE
+
+    show_logs()
+
+
+
+
---
+++ project/services.py
@@ -0,0 +1,169 @@
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from itertools import islice
+from datetime import datetime
+import pandas as pd
+import streamlit as st
+import logging
+import os
+
+files = os.listdir('./data')
+workbooks = [item for item in files if '.xlsx' in item]
+
+logging.basicConfig(filename='log.log', filemode='w', format='%(asctime)s - %(levelname)s %(message)s', datefmt='%H:%M:%S', encoding='utf-8', level=logging.DEBUG, force=True)
+
+months = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06', 'July': '07',
+          'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12', 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07',
+          'Aug': '08', 'Sep': '09', 'Oc': '10', 'Nov': '11', 'Dec': '12'}
+years = ['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
+
+def check_file(file):
+    """
+    Checks if file exists and logs it.
+    """
+    while True:
+        try:
+            file = f'data/{file}'
+            if os.path.exists(file):
+                logging.info(f"{file} exists")
+                break
+        except FileNotFoundError:
+            print("FileNotFound: not a valid file.")
+            logging.warning("FileNotFound: not a valid file.")
+        else:
+            continue
+
+
+def get_summary(ws, month_year_format):
+    """
+    Grabs relevant data from Summary MoM sheet.
+    """
+    row = None
+    for item in ws['A']:
+        if month_year_format in str(item.value):
+            row = item.row
+            st.write(f'(Row: {row})')
+    values = [ro for ro in ws.iter_rows(min_row=row, max_row=row, values_only=True)]
+    new_values = [item for item in values[0][1:] if item != None]
+
+    # create dictionary from row data
+    row_data = {}
+    row_data['30s_abandonment'] = f'Abandon after 30s: {round(new_values[1]*100,2)}%'
+    row_data['fcr'] = f'FCR : {new_values[2]*100}0%'
+    row_data['dsat'] = f'DSAT : {new_values[3]*100}0%'
+    row_data['csat'] = f'CSAT : {new_values[4]*100}0%'
+
+    logging.info('get_summary succesful')
+
+    return row_data
+
+
+def nps_check(type, number):
+    """
+    Check size of group and return 'GOOD' or 'BAD'.
+    """
+    if type == 'promoters':
+        if number >= 200:
+            return 'GOOD'
+        else:
+            return 'BAD'
+    if type == 'passives':
+        if number >= 100:
+            return 'GOOD'
+        else:
+            return 'BAD'
+    if type == 'detractors':
+        if number < 100:
+            return 'GOOD'
+        else:
+            return 'BAD'
+        
+        
+def get_voc(ws, month_year_format):
+    """
+    Grabs relevant data from VOC MoM sheet.
+    """
+    col = None
+    for item in ws[1]:
+        if month_year_format in str(item.value):
+            col = item.column
+            st.write(f'(Column: {col})')
+    values = [co for co in ws.iter_cols(min_col=col, max_col=col, values_only=True)]
+    new_values = [item for item in values[0][1:] if item != None and isinstance(item, int)]
+
+    # create dictionary from column data
+    col_data = {}
+    col_data['base'] = f'Base Size: {new_values[0]}'
+    col_data['promoters'] = [f'Promoters: {new_values[1]}', nps_check('promoters', new_values[1])]
+    col_data['passives'] = [f'Passives: {new_values[2]}', nps_check('passives', new_values[2])]
+    col_data['detractors'] = [f'Detractors: {new_values[3]}', nps_check('detractors', new_values[3])]
+
+    logging.info('get_voc succesful')
+
+    return col_data
+    
+
+def get_current():
+    """
+    Grabs the current month in integer / string formats and year.
+    """
+    # format month year for datetime comparison
+    month = datetime.now().strftime('%m')
+    month_word = datetime.now().strftime('%B')
+    year = datetime.now().year
+
+    logging.info(f'Current: {month_word}, {month}-{year}')
+
+    return month, month_word, year
+    
+
+
+def log_summary(row_data):
+    """
+    Log Summary data.
+    """
+    print(row_data)
+    for item in row_data:
+        logging.info(row_data[item])
+
+
+def log_voc(col_data):
+    """
+    Log VOC data.
+    """
+    for item in col_data:
+        if 'base' in item:
+            logging.info(col_data[item])
+        else:
+            logging.info(f'{col_data[item][0]} - {col_data[item][1]}')
+
+
+def show_summary(row_data):
+    """
+    Display Summary data in streamlit app.
+    """
+    for item in row_data:
+        st.write(row_data[item])
+
+    logging.info(f'Displayed summary in app')
+
+
+def show_voc(col_data):
+    """
+    Display VOC data in streamlit app.
+    """
+    for item in col_data:
+        if 'base' in item:
+            st.write(col_data[item])
+        else:
+            st.write(f'{col_data[item][0]} - {col_data[item][1]}')
+
+    logging.info(f'Displayed voc in app')
+
+
+def show_logs():
+    with open('log.log') as log_file:
+        for line in log_file:
+            st.write(line)
+
+    logging.info('Viewed logs')
